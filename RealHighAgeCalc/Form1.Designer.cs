7b480a66b9ed5ceef350a2ceeba547064bec93d4
Brijesh Kamani <brijeshkamani11@gmail.com>
2018-02-15 17:38:53 +0530

    Helllo

---
+++ RealHighAgeCalc/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace RealHighAgeCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtp = new System.Windows.Forms.DateTimePicker();
+            this.lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtp
+            // 
+            this.dtp.Location = new System.Drawing.Point(24, 28);
+            this.dtp.Name = "dtp";
+            this.dtp.Size = new System.Drawing.Size(200, 22);
+            this.dtp.TabIndex = 0;
+            this.dtp.ValueChanged += new System.EventHandler(this.dtp_ValueChanged);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(33, 104);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 17);
+            this.lbl.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 253);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.dtp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtp;
+        private System.Windows.Forms.Label lbl;
+    }
+}
+
---
+++ RealHighAgeCalc/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealHighAgeCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dtp.MaxDate = System.DateTime.Now;
+        }
+
+        private void dtp_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime now = System.DateTime.Now;
+            DateTime selected = dtp.Value;
+            int OldMonth, year = 0, Month = 0, Day = 0;
+            OldMonth = now.Month;
+            while (OldMonth == now.Month)
+            {
+                now = now.AddDays(-1);
+                selected = selected.AddDays(-1);
+
+            }
+            while (now.CompareTo(selected) >= 0)
+            {
+                year++;
+                now = now.AddYears(-1);
+
+            }
+            year--;
+            now = now.AddYears(1);
+            OldMonth = now.Month;
+            while (now.CompareTo(selected) >= 0)
+            {
+                Day++;
+                now = now.AddDays(-1);
+                if (OldMonth != now.Month && now.CompareTo(selected) >= 0)
+                {
+                    Month++;
+                    Day = 0;
+                    OldMonth = now.Month;
+                }
+            }
+            now = now.AddDays(1);
+            Day--;
+            TimeSpan dif = now.Subtract(selected);
+
+            
+            lbl.Text = "The Difference Is " + year + " Years " + Month + " Month " + Day + " Days ";
+           
+
+
+        }
+    }
+}
